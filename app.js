f6ca977b28793d015a7e5f77f562f966e4c04fe3
RYAN ALVES DE CARVALHO <23111765@CFP127ALUNO.LOCAL>
2023-12-13 17:12:49 -0300

    cors

--- app.js
+++ app.js
@@ -23,106 +23,96 @@ app.use((request, response, next) => {
 
 })  
 
+
+// EndPoints: Listar o nome, id, email e senha de todos os usuários
 app.get('/usuarios/', cors(), async (request, response, next) => {
-    try {
-        const listaUsuarios = funcoes.getListaUsuarios();
-        response.status(200).json(listaUsuarios);
-    } catch (error) {
-        console.error(error);
-        response.status(500).json({ erro: 'Erro ao processar a solicitação' });
-    }
-});
-
-
-// // EndPoints: Listar o nome, id, email e senha de todos os usuários
-// app.get('/usuarios/', cors(), async (request, response, next) => {
-//     response.status(200)
-//     response.json(funcoes.getListaUsuarios())
+    response.status(200)
+    response.json(funcoes.getListaUsuarios())
     
+    
+})
+
+// // EndPoints: Listar os dados de um usuário
+// app.get('/usuario/:id', cors(), async (request, response, next) => {
+
+//     let id = request.params.id
+
+//     if(funcoes.getInfoUsuario(id)){
+//         response.json(funcoes.getInfoUsuario(id))
+//         response.status(200)
+//     }else{
+//         response.json({erro: 'Não foi possível encontrar um item'})
+//         response.status(404)
+//     }
     
 // })
 
-// EndPoints: Listar os dados de um usuário
-app.get('/usuario/:id', cors(), async (request, response, next) => {
+// // EndPoints: Listar o nome e imagem das categorias
+// app.get('/categorias/', cors(), async (request, response, next) => {
 
-    let id = request.params.id
-
-    if(funcoes.getInfoUsuario(id)){
-        response.json(funcoes.getInfoUsuario(id))
-        response.status(200)
-    }else{
-        response.json({erro: 'Não foi possível encontrar um item'})
-        response.status(404)
-    }
+//     response.json(funcoes.getListaCategorias())
+//     response.status(200)
     
-})
+// })
 
-// EndPoints: Listar o nome e imagem das categorias
-app.get('/categorias/', cors(), async (request, response, next) => {
+// // EndPoints: Listar o nome, imagem e preço das pizzas
+// app.get('/pizzas/', cors(), async (request, response, next) => {
 
-    response.json(funcoes.getListaCategorias())
-    response.status(200)
+//     response.json(funcoes.getListaPizzas())
+//     response.status(200)
     
-})
+// })
 
-// EndPoints: Listar o nome, imagem e preço das pizzas
-app.get('/pizzas/', cors(), async (request, response, next) => {
+// app.get('/pizzas/favoritas', cors(), async (request, response, next) => {
 
-    response.json(funcoes.getListaPizzas())
-    response.status(200)
+//     response.json(funcoes.getListaPizzasFavoritas())
+//     response.status(200)
     
-})
+// })
 
-app.get('/pizzas/favoritas', cors(), async (request, response, next) => {
+// // EndPoints: Listar as informações de uma pizza
+// app.get('/pizza/:id', cors(), async (request, response, next) => {
 
-    response.json(funcoes.getListaPizzasFavoritas())
-    response.status(200)
+//     let id = request.params.id
+
+//     if(funcoes.getInfoPizza(id)){
+//         response.json(funcoes.getInfoPizza(id))
+//         response.status(200)
+//     }else{
+//         response.json({erro: 'Não foi possível encontrar um item'})
+//         response.status(404)
+//     }
     
-})
+// })
 
-// EndPoints: Listar as informações de uma pizza
-app.get('/pizza/:id', cors(), async (request, response, next) => {
+// // EndPoints: Listar os comentários de uma pizza
+// app.get('/comentarios/:id', cors(), async (request, response, next) => {
 
-    let id = request.params.id
+//     let id = request.params.id
 
-    if(funcoes.getInfoPizza(id)){
-        response.json(funcoes.getInfoPizza(id))
-        response.status(200)
-    }else{
-        response.json({erro: 'Não foi possível encontrar um item'})
-        response.status(404)
-    }
+//     if(funcoes.getComentarios(id)){
+//         response.json(funcoes.getComentarios(id))
+//         response.status(200)
+//     }else{
+//         response.json({erro: 'Não foi possível encontrar um item'})
+//         response.status(404)
+//     }
     
-})
+// })
 
-// EndPoints: Listar os comentários de uma pizza
-app.get('/comentarios/:id', cors(), async (request, response, next) => {
+// // EndPoints: Listar as informações de todas as bebidas
+// app.get('/bebidas/', cors(), async (request, response, next) => {
 
-    let id = request.params.id
+//     let id = request.params.id
 
-    if(funcoes.getComentarios(id)){
-        response.json(funcoes.getComentarios(id))
-        response.status(200)
-    }else{
-        response.json({erro: 'Não foi possível encontrar um item'})
-        response.status(404)
-    }
+//     if(funcoes.getListaBebidas(id)){
+//         response.json(funcoes.getListaBebidas(id))
+//         response.status(200)
+//     }else{
+//         response.json({erro: 'Não foi possível encontrar um item'})
+//         response.status(404)
+//     }
     
-})
-
-// EndPoints: Listar as informações de todas as bebidas
-app.get('/bebidas/', cors(), async (request, response, next) => {
-
-    let id = request.params.id
-
-    if(funcoes.getListaBebidas(id)){
-        response.json(funcoes.getListaBebidas(id))
-        response.status(200)
-    }else{
-        response.json({erro: 'Não foi possível encontrar um item'})
-        response.status(404)
-    }
-    
-})
+// })
 
 app.listen(8080, () => {})
